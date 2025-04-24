aee4f97a957bd649db24db28be3093566f90dfd5
gitsergio99 <chernikov1981sv@gmail.com>
2025-04-24 11:46:04 +0700

    modified:   crq_lib.nim 	modified:   test.nim

--- crq_lib.nim
+++ crq_lib.nim
@@ -1,4 +1,5 @@
-import std/[tables,sequtils,strformat, httpclient, base64, strutils]
+import std/[tables,strformat, httpclient, base64, strutils]
+
 
 type
     Crq_value* = object
@@ -25,14 +26,43 @@ proc get_current_values_crq*(params:seq[string], url: string, user: string, pass
     if param_count>1:
         for x in countup(1,param_count-2):
             tmp_seq = resp_seq[x].split(", ")
-            res_table[tmp_seq[0]] = Crq_value(time:tmp_seq[1],value:tmp_seq[2],state:tmp_seq[3])
+            res_table[tmp_seq[0]] = Crq_value(time:tmp_seq[1][0..^2],value:tmp_seq[2],state:tmp_seq[3])
     result = res_table
 
-proc get_spec_info*(req:string, url: string, user: string, pass: string): string =
+proc get_spec_info*(req: string, url: string, user: string, pass: string): string =
+    #[
+    some usefull req for ecom 3000:
+        dev_info - information about ecom 3000;
+        cpuuse - current cpu load of ecom 300-;
+        events - events by chanels;
+        gettime - current time at ecom 3000;
+        ident - identificator of ecom 3000;
+        last_event;
+        sys_events - system events;
+        version - version of system software ecom 3000
+    ]#
     var
         http_C = newHttpClient()
         get_req: string = url & fmt"/crq?req={req}"
     if user == "":
         http_C.headers["Authorization"] = "Basic " & base64.encode(user & ":" & pass)
-    
+    get_req.add(req)
     result = http_C.getContent(get_req)
+
+proc get_arch_crq*(param: string, start_t: string, end_t: string, url: string, user: string, pass: string): Table[string, Crq_value] =
+    var
+        http_C = newHttpClient()
+        get_req: string = url & fmt"/crq?req=archive&type={param[0]}&n1={param[1..^1]}&{param[0]}&n2={param[1..^1]}&t1={start_t}&t2={end_t}"
+        res_table = initTable[string, Crq_value]()
+        resp_seq: seq[string]
+        tmp_seq: seq[string]
+        param_count: int
+    if user == "":
+        http_C.headers["Authorization"] = "Basic " & base64.encode(user & ":" & pass)
+    resp_seq = http_C.getContent(get_req).splitLines()
+    param_count = len(resp_seq)
+    if param_count>1:
+        for x in countup(1,param_count-2):
+            tmp_seq = resp_seq[x].split(", ")
+            res_table[tmp_seq[1][0..^2]] = Crq_value(time:tmp_seq[1][0..^2],value:tmp_seq[2],state:tmp_seq[3])
+    result = res_table
--- test.nim
+++ test.nim
@@ -1,6 +1,12 @@
-import crq_lib,std/[tables]
+import crq_lib,std/[tables,strutils]
 var
     res = initTable[string, Crq_value]()
+    #x = Ecom3000_info.dev_info
+    tmp_str:string = "G12"
 
-res = get_current_values_crq(@["g1","g10","g18"],"http://10.39.15.55","","")
-echo res["G1"]
+echo tmp_str[1..^1]
+
+#res = get_current_values_crq(@["g1","g10","g18"],"http://10.39.15.55","","")
+#echo res["G1"]
+
+echo get_arch_crq("g17","202503100000000","202503110000000","http://10.39.15.55","","")
